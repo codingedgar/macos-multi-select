bcc8c1fd3a147ac17f9317503eec88ca29b56124
codingedgar <codingedgar@gmail.com>
2021-08-25 11:04:02 +0200

    add demo #28

---
+++ demo/main.js
@@ -0,0 +1,88 @@
+
+import { multiselect, Context } from '../dist/index'
+// console.log(index)
+let context = {
+  index: [],
+  selected: [],
+  adjacentPivot: undefined
+}
+
+let shiftPressed = false;
+let controlPressed = false;
+
+/**
+ * 
+ * @param {Context} command 
+ */
+function update(command) {
+  context = multiselect(context, command);
+
+  context.index.forEach(key => {
+    const row = document.getElementById(key);
+    if (context.selected.indexOf(key) !== -1) {
+      row.classList.add("selected");
+    } else {
+      row.classList.remove("selected");
+    }
+  })
+}
+
+var list = document.getElementById("list");
+
+window.onkeydown = (e => {
+  if (e.key === "Meta" || e.key === "Control") {
+    controlPressed = true
+  }
+  if (e.key === "Shift") {
+    shiftPressed = true
+  }
+  if(e.key === "ArrowDown") {
+    update({
+      type: "SELECT NEXT"
+    });
+  }
+  if(e.key === "ArrowUp") {
+    update({
+      type: "SELECT PREVIOUS"
+    });
+  }
+})
+
+
+window.onkeyup = (e => {
+  if (e.key === "Meta" || e.key === "Control") {
+    controlPressed = false
+  }
+  if (e.key === "Shift") {
+    shiftPressed = false
+  }
+})
+
+for (let index = 0; index < 10; index++) {
+  var item = document.createElement("li");
+  item.id = index;
+  item.onclick = (e) => {
+    console.log(controlPressed, shiftPressed)
+    if (controlPressed) {
+      update({
+        type: "TOGGLE SELECTION",
+        id: index.toString()
+      })
+    } else if (shiftPressed) {
+      update({
+        type: "SELECT ADJACENT",
+        id: index.toString()
+      })
+    } else {
+      update({
+        type: "SELECT ONE",
+        id: index.toString()
+      })
+    }
+  }
+  const span = document.createElement("span");
+  span.innerHTML = `Row ${index}`;
+  item.appendChild(span);
+  list.appendChild(item);
+  context.index.push(index.toString());
+}
