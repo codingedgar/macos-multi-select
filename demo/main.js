22f6b9ccf5e6cc43d0604117fa0d582d93d73342
codingedgar <codingedgar@gmail.com>
2021-08-28 20:48:09 +0200

    select all #5

--- demo/main.js
+++ demo/main.js
@@ -1,4 +1,5 @@
-import { multiselect, Context } from '../dist/index'
+// @ts-check
+import { multiselect, Command } from '../dist/index'
 
 let context = {
   index: [],
@@ -8,10 +9,11 @@ let context = {
 
 let shiftPressed = false;
 let controlPressed = false;
+let altPressed = false;
 
 /**
  * 
- * @param {Context} command 
+ * @param {Command} command
  */
 function update(command) {
   context = multiselect(context, command);
@@ -28,12 +30,15 @@ function update(command) {
 
 var list = document.getElementById("list");
 
-window.onkeydown = (e => {
+window.addEventListener("keydown", e => {
   if (e.key === "Meta" || e.key === "Control") {
     controlPressed = true
   }
   if (e.key === "Shift") {
     shiftPressed = true
+  }
+  if (e.key === "Alt") {
+    altPressed = true
   }
   if (shiftPressed && e.key === "ArrowDown") {
     update({
@@ -43,30 +48,46 @@ window.onkeydown = (e => {
     update({
       type: "SELECT PREVIOUS ADJACENT"
     });
-  } else if (e.key === "ArrowDown") {
-    update({
-      type: "SELECT NEXT"
-    });
   } else if (e.key === "ArrowUp") {
     update({
       type: "SELECT PREVIOUS"
     });
+  } else if (e.key === "ArrowDown") {
+    update({
+      type: "SELECT NEXT"
+    });
+  } else if (e.key === "a" && controlPressed && altPressed) {
+    e.preventDefault();
+    e.stopPropagation();
+    update({
+      type: "DESELECT ALL"
+    });
+  } else if (e.key === "a" && controlPressed) {
+    e.preventDefault();
+    e.stopPropagation();
+    update({
+      type: "SELECT ALL"
+    });
   }
-})
+}, true);
 
 
 window.onkeyup = (e => {
   if (e.key === "Meta" || e.key === "Control") {
-    controlPressed = false
+    controlPressed = false;
   }
   if (e.key === "Shift") {
-    shiftPressed = false
+    shiftPressed = false;
+  }
+
+  if (e.key === "Alt") {
+    altPressed = false;
   }
 })
 
 for (let index = 0; index < 10; index++) {
   var item = document.createElement("li");
-  item.id = index;
+  item.id = index.toString();
   item.onclick = (e) => {
     if (controlPressed) {
       update({
@@ -91,3 +112,58 @@ for (let index = 0; index < 10; index++) {
   list.appendChild(item);
   context.index.push(index.toString());
 }
+
+const buttons = document.getElementById("buttons");
+const deselectAllButton = document.createElement("button");
+deselectAllButton.innerText = "Deselect All (command + alt + a)";
+deselectAllButton.onclick = () => {
+  update({
+    type: "DESELECT ALL"
+  });
+};
+buttons.appendChild(deselectAllButton);
+
+const selectAllButton = document.createElement("button");
+selectAllButton.innerText = "Select All (command + a)";
+selectAllButton.onclick = () => {
+  update({
+    type: "SELECT ALL"
+  });
+};
+buttons.appendChild(selectAllButton);
+
+const nextButton = document.createElement("button");
+nextButton.innerText = "Next (arrow down)";
+nextButton.onclick = () => {
+  update({
+    type: "SELECT NEXT"
+  });
+};
+buttons.appendChild(nextButton);
+
+const previousButton = document.createElement("button");
+previousButton.innerText = "Previous (arrow up)";
+previousButton.onclick = () => {
+  update({
+    type: "SELECT PREVIOUS"
+  });
+};
+buttons.appendChild(previousButton);
+
+const nextAdjacentButton = document.createElement("button");
+nextAdjacentButton.innerText = "Next Adjacent (shift + arrow down)";
+nextAdjacentButton.onclick = () => {
+  update({
+    type: "SELECT NEXT ADJACENT"
+  });
+};
+buttons.appendChild(nextAdjacentButton);
+
+const previousAdjacentButton = document.createElement("button");
+previousAdjacentButton.innerText = "Previous Adjacent (shift + arrow up)";
+previousAdjacentButton.onclick = () => {
+  update({
+    type: "SELECT PREVIOUS ADJACENT"
+  });
+};
+buttons.appendChild(previousAdjacentButton);
